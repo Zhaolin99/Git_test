4a24400a5766e95d1a599856742ee18daae15648
Zhaolin <zlzhong8j@gmail.com>
2022-03-28 18:52:17 -0700

    first change

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main () {
+  int ch;
+
+  printf("its a change!");
+  for( ch = 75 ; ch <= 100; ch++ ) {
+    printf("ASCII value = %d, Character = %c\n", ch , ch );
+  }
+
+
+  return(0);
+}
